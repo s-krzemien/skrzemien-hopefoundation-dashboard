02e9e0c5f891d6336a94e66da47f9cd99fb6fcf4
s-krzemien <shelbykrz3@gmail.com>
2025-05-12 22:35:56 -0500

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -275,11 +275,10 @@ elif page == "Grant Utilization Overview":
     ).get_figure()
     st.pyplot(fig)
 
+    st.subheader("Support by Assistance Type")
     assistance_support= stdf.groupby("assistance_type")["amount"].sum()
     st.write(assistance_support)
-    st.bar_chart(assistance_support)
-    st.subheader("Support by Assistance Type")
-
+    
 
  # Impact & Progress Summary 
 elif page == "Impact & Progress Summary":
